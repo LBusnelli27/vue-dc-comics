02c5dd5a19833b21ec7313e5ccf8aff407cafec3
Busna27 <lucabusnelli2001@gmail.com>
2022-03-28 16:24:53 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,102 @@
   <div class="header">
     <img src="../assets/img/dc-logo.png" alt="Logo DC Comics Header">
 
-    <ul>
-      <li></li>
-    </ul>
+    <div class="nav-container">
+      <ul>
+        <li v-for="(element, index) in navHeaderElement" :key="index">
+          <a :href="element.href">{{ element.text }}</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'indexHeader',
+  data : function() {
+    return {
+      navHeaderElement : [
+        {
+          text: "Characters",
+          href: "#",
+          active: false
+        },
+        {
+          text: "Comics",
+          href: "#",
+          active: true
+        },
+        {
+          text: "Movies",
+          href: "#",
+          active: false
+        },
+        {
+          text: "Tv",
+          href: "#",
+          active: false
+        },
+        {
+          text: "Games",
+          href: "#",
+          active: false
+        },
+        {
+          text: "Collectibles",
+          href: "#",
+          active: false
+        },
+        {
+          text: "Videos",
+          href: "#",
+          active: false
+        },
+        {
+          text: "Fans",
+          href: "#",
+          active: false
+        },
+        {
+          text: "News",
+          href: "#",
+          active: false
+        },
+        {
+          text: "Shop",
+          href: "#",
+          active: false
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  @import "../assets/scss/style.scss";
 
+  .header{
+    @include centerContentInDiv;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 0;
+    height: 10vh;
+
+    .nav-container {
+      @include centerContentInDiv;
+      height: 100%;
+      ul{
+        display: flex;
+        li {
+          padding: 0 .6rem;
+          text-transform: uppercase;
+          font-size: .8rem;
+          &:hover {
+            border-bottom: 3px solid $mainColor;
+          }
+        }
+      }
+    }
+  }
 </style>
