1ac31e6ecdee2f3073f4f7ddb9167c76bd1549c7
Busna27 <lucabusnelli2001@gmail.com>
2022-03-28 15:06:42 +0200

    iniz. dc comics

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="../img/dc-logo.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'indexHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
