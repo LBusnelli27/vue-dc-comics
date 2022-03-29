5552547f54d7dafbd4d572baf1b058d4b81784cf
Busna27 <lucabusnelli2001@gmail.com>
2022-03-29 17:07:36 +0200

    card comics section

--- src/components/CardComic.vue
+++ src/components/CardComic.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-    
+  <div class="comic-element">
+    <img :src="bookObj.thumb" :alt="bookObj.series">
+    <p>{{ bookObj.series }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'CardComic',
+  props: ['bookObj']
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .comic-element {
+    width: calc((100% / 6) - 1.5rem);
+    img{
+      width: 100%;
+    }
+    p{
+      color: white;
+      text-transform: uppercase;
+      font-size: .8rem;
+    }
+  }
 </style>
