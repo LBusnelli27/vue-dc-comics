36131aebf5221b5f0a87f04bebd54c8cf8d2d6dc
Busna27 <lucabusnelli2001@gmail.com>
2022-03-29 16:11:43 +0200

    jumbotron

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="header">
-    <img src="../assets/img/dc-logo.png" alt="Logo DC Comics Header">
+    <section>
+      <img src="../assets/img/dc-logo.png" alt="Logo DC Comics Header">
+      <div class="nav-container">
+        <ul>
+          <li v-for="(element, index) in navHeaderElement" :key="index">
+            <a :href="element.href">{{ element.text }}</a>
+          </li>
+        </ul>
+      </div>
+    </section>
 
-    <div class="nav-container">
-      <ul>
-        <li v-for="(element, index) in navHeaderElement" :key="index">
-          <a :href="element.href">{{ element.text }}</a>
-        </li>
-      </ul>
+    <div class="jumbotron">
     </div>
   </div>
 </template>
@@ -78,32 +82,38 @@ export default {
   @import "../assets/scss/style.scss";
 
   .header{
-    @include withWebsite;
-    @include centerContentInDiv;
-    display: flex;
-    justify-content: space-between;
-    padding: 1rem 0;
-
-    img {
-      height: 80px;
-      width: 80px;
-    }
-
-    .nav-container {
+    section {
+      @include withWebsite;
       @include centerContentInDiv;
-      height: 100%;
-      ul{
-        display: flex;
-        li {
-          padding: 0 .6rem;
-          text-transform: uppercase;
-          font-size: .8rem;
-          font-weight: 600;
-          &:hover {
-            border-bottom: 3px solid $mainColor;
+      display: flex;
+      justify-content: space-between;
+      padding: 1rem 0;
+      img {
+        height: 80px;
+        width: 80px;
+      }
+      .nav-container {
+        @include centerContentInDiv;
+        height: 100%;
+        ul{
+          display: flex;
+          li {
+            padding: 0 .6rem;
+            text-transform: uppercase;
+            font-size: .8rem;
+            font-weight: 600;
+            &:hover {
+              border-bottom: 3px solid $mainColor;
+            }
           }
         }
       }
+    }
+
+    .jumbotron {
+      height: 30vh;
+      background-image: url(../assets/img/jumbotron.jpg);
+      background-size: cover;
     }
   }
 </style>
